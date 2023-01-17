94c9d79c802e17cdee67858676453adfe538d06f
Arya Burhan <99354272+AaronPaul6@users.noreply.github.com>
2023-01-17 12:01:16 +0300

    commit3

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -34,7 +34,7 @@ class _LoginScreenState extends State<LoginScreen> {
         body: Column(
           children: [
             Container(
-              color: Colors.lime,
+              color: Colors.pink,
               width: 100.w,
               height: 35.h,
               child: Row(
