d433df46adea5a7e7877d71aab92cdd222ce54df
marzeroman <marfsaman12345@gmail.com>
2023-01-23 00:34:14 +0300

    commit5

---
+++ lib/screens/home_screen/widgets/about_us.dart
@@ -0,0 +1,6 @@
+import 'package:brain_school/constants.dart';
+import 'package:brain_school/screens/my_profile/my_profile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sizer/sizer.dart';
+import 'widgets/student_data.dart';
