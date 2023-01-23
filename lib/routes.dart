cb9e4d4ca8c120a04dd51f7459fab65ad8546846
marzeroman <marfsaman12345@gmail.com>
2023-01-23 10:25:43 +0300

    test

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,7 +3,7 @@ import 'dart:js';
 import 'package:brain_school/screens/login_screen/login_screen.dart';
 import 'package:brain_school/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
-import 'screens/home_screen/about_us.dart';
+import 'screens/about_us/about_us.dart';
 import 'screens/home_screen/home_screen.dart';
 import 'screens/my_profile/my_profile.dart';
 
@@ -13,6 +13,5 @@ Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => LoginScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
   MyProfileScreen.routeName: (context) => MyProfileScreen(),
-  AboutUs.routeName: (context)=> AboutUs(),
+  AboutUs.routeName: (context) => AboutUs(),
 };
-
--- lib/screens/about_us/about_us.dart
+++ lib/screens/about_us/about_us.dart
@@ -1,9 +1,9 @@
+// TODO Implement this library.
 import 'package:brain_school/constants.dart';
 import 'package:brain_school/screens/my_profile/my_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sizer/sizer.dart';
-import 'widgets/student_data.dart';
 
 
 class AboutUs extends StatelessWidget {
