67adcca91e4a78a16f55d3a06c5873607d57d849
marzeroman <marfsaman12345@gmail.com>
2023-01-24 11:43:39 +0300

    change_by_marf

--- lib/screens/about_us/about_us.dart
+++ lib/screens/about_us/about_us.dart
@@ -51,7 +51,7 @@ class _AboutUs extends State<AboutUs> {
                     children: [
                       Text('ABOUT US',
                           style: Theme.of(context).textTheme.subtitle1),
-                      Text('Kurdistan Technical Institute',
+                      Text('Kurdistan Technical Institute OF SELMANI',
                           style: Theme.of(context).textTheme.subtitle2),
                       sizedBox,
                     ],
