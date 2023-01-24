c293cb5fb4a36f793066773c120297b24c586e5f
marzeroman <marfsaman12345@gmail.com>
2023-01-24 10:24:08 +0300

    change_by_marf

--- lib/screens/about_us/about_us.dart
+++ lib/screens/about_us/about_us.dart
@@ -4,6 +4,11 @@ import 'package:brain_school/screens/my_profile/my_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sizer/sizer.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+
 
 
 class AboutUs extends StatelessWidget {
@@ -11,8 +16,38 @@ class AboutUs extends StatelessWidget {
   static String routeName = 'ABoutUs';
 
   @override
+  _AboutUs createState() => _AboutUs();
+  }
+  
+  class _AboutUs extends State<AboutUs> {
+    final _unfocusNode = FocusNode();
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
+    @override
+    void dispose() {
+      _unfocusNode.dispose();
+      super.dispose();
+    }
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        automaticallyImplyLeading: false,
+        title: Text(
+          'About Us',
+            style: FlutterFlowTheme.of(context).title2.override(
+              fontFamily: 'Poppins',
+              color: Colors.white,
+              fontSize: 22,
+            ),
+        ),
+      ),
+      ),
+
     throw UnimplementedError();
   }
-}
+
