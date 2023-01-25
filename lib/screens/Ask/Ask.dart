d0263590219c503fd36afeb1d6d6dc7ba4995ba5
ashnausuf <ashna.usuf27@gmail.com>
2023-01-25 11:03:46 +0300

    commit_ashna

--- lib/screens/Ask/Ask.dart
+++ lib/screens/Ask/Ask.dart
@@ -50,24 +50,4 @@ class _Ask extends State<Ask> {
     );
   }
 
-  TextFormField buildEmailField() {
-    return TextFormField(
-      textAlign: TextAlign.start,
-      keyboardType: TextInputType.emailAddress,
-      style: kInputTextStyle,
-      decoration: InputDecoration(
-        labelText: 'Mobile Number/Email',
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-      ),
-      validator: (value) {
-        //for validation
-        RegExp regExp = new RegExp(emailPattern);
-        if (value == null || value.isEmpty) {
-          return 'Please enter some text';
-        } else if (!regExp.hasMatch(value)) {
-          return 'Please enter a valid email address';
-        }
-      },
-    );
   }
-}
