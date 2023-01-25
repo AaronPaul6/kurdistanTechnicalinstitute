02d243f2d52e04d0524f641108b2b3d2af7d37f3
AlandAso11 <aaqu180035@uniq.edu.iq>
2023-01-25 12:25:13 +0300

    we have creat a new page

---
+++ lib/screens/gallery/gallery.dart
@@ -0,0 +1,34 @@
+import 'package:brain_school/constants.dart';
+import 'package:brain_school/screens/my_profile/my_profile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sizer/sizer.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:brain_school/components/custom_buttons.dart';
+import 'package:brain_school/constants.dart';
+import 'package:brain_school/screens/home_screen/home_screen.dart';
+import 'package:sizer/sizer.dart';
+
+
+class Gallery extends StatefulWidget {
+  static String routeName = 'gallery_page';
+
+  @override
+  gallery createState() => _Gallery();
+
+  _Gallery() {}
+}
+
+class _gallery {
+}
+
+class gallery extends State<Gallery> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
