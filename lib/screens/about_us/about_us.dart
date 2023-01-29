511155debba7d4eccd5a68dcaeb980c31c8bf830
marzeroman <marfsaman12345@gmail.com>
2023-01-29 11:28:03 +0300

    change_by_marf

--- lib/screens/about_us/about_us.dart
+++ lib/screens/about_us/about_us.dart
@@ -25,112 +25,118 @@ class _AboutUs extends State<AboutUs> {
   //changes current state
   @override
   void initState() {
-
     // super.initState();
- //   _ = true;
+    //   _ = true;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      //when user taps anywhere on the screen, keyboard hides
-      appBar: AppBar(
-
-        centerTitle: true,
-        title: Text(
-          'ABOUT US',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        automaticallyImplyLeading: false,
-        leadingWidth: 100,
-        leading: ElevatedButton.icon(
-          onPressed: () => Navigator.of(context).pop(),
-          icon: const Icon(Icons.arrow_left_sharp),
-          label: const Text('Back'),
-          style: ElevatedButton.styleFrom(
-            elevation: 0,
-            backgroundColor: Colors.blue.shade800,
+        backgroundColor: Colors.white,
+        //when user taps anywhere on the screen, keyboard hides
+        appBar: AppBar(
+          // Overide the default Back button
+          backgroundColor: Colors.blue.shade800,
+          centerTitle: true,
+          title: Text(
+            'ABOUT US',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          automaticallyImplyLeading: false,
+          leadingWidth: 100,
+          leading: ElevatedButton.icon(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: const Icon(Icons.arrow_left_sharp),
+            label: const Text('Back'),
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              backgroundColor: Colors.blue.shade800,
+            ),
           ),
         ),
-
-      ),
         body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Image.asset('assets/images/kti_image.jpg',width: 416.6,height: 239.9,fit: BoxFit.cover,),
-              Expanded(
-                child: Text(
-                  'The Technical Institute of Kurdistan, established on 19/10/2015 by the ministerial decree No. 21923, wants to take part in this historical and national responsibility. A leading institute, although the Kurdistan Technical Institute was established in a very timely manner, meant it was a necessity for the stage, as Kurdistan’s students needed a modern institute with a modern program.In this way, the Institute insists on '
-                      'implementing a protocol of scientific and cultural understanding with universities and institutes inside and outside the region and around the world, and has so far signed agreements with several universities and institutes. All of this, the Kurdistan Technical Institute believes that the students are the'
-                      ' center of the learning process, so it insists on providing the students with the ability to provide new equipment and technology for the students of the institute according to the stage and the era. To provide and develop the skills and science of students, in a way that leads to the development of science and technology in the region, so that it can serve the homeland from here and achieve the desires of the nation.'
+            child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Image.asset(
+              'assets/images/kti_image.jpg',
+              width: 416.6,
+              height: 239.9,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+                height: 300,
+                child:Column(
+                  children:[
+                    Expanded(
+                       child: Text(
+                    'The Technical Institute of Kurdistan, established on 19/10/2015 by the ministerial decree No. 21923, wants to take part in this historical and national responsibility. A leading institute, although the Kurdistan Technical Institute was established in a very timely manner, meant it was a necessity for the stage, as Kurdistan’s students needed a modern institute with a modern program.In this way, the Institute insists on '
+                    'implementing a protocol of scientific and cultural understanding with universities and institutes inside and outside the region and around the world, and has so far signed agreements with several universities and institutes. All of this, the Kurdistan Technical Institute believes that the students are the'
+                    ' center of the learning process, so it insists on providing the students with the ability to provide new equipment and technology for the students of the institute according to the stage and the era. To provide and develop the skills and science of students, in a way that leads to the development of science and technology in the region, so that it can serve the homeland from here and achieve the desires of the nation.'),
+                    ),
+          ],
+                            ),
+                      ),
 
-                ),
 
-           )
-            ],
-          )
-
-
-        )
-    );
+                      ],
+        )));
   }
 
-  // TextFormField buildEmailField() {
-  //   return TextFormField(
-  //     textAlign: TextAlign.start,
-  //     keyboardType: TextInputType.emailAddress,
-  //     style: kInputTextStyle,
-  //     decoration: InputDecoration(
-  //       labelText: 'Mobile Number/Email',
-  //       floatingLabelBehavior: FloatingLabelBehavior.always,
-  //     ),
-  //     validator: (value) {
-  //       for validation
-        // RegExp regExp = new RegExp(emailPattern);
-        // if (value == null || value.isEmpty) {
-        //   return 'Please enter some text';
-        //   if it does not matches the pattern, like
-          // it not contains @
-        // } else if (!regExp.hasMatch(value)) {
-        //   return 'Please enter a valid email address';
-        // }
-      // },
-    // );
-  // }
+// TextFormField buildEmailField() {
+//   return TextFormField(
+//     textAlign: TextAlign.start,
+//     keyboardType: TextInputType.emailAddress,
+//     style: kInputTextStyle,
+//     decoration: InputDecoration(
+//       labelText: 'Mobile Number/Email',
+//       floatingLabelBehavior: FloatingLabelBehavior.always,
+//     ),
+//     validator: (value) {
+//       for validation
+// RegExp regExp = new RegExp(emailPattern);
+// if (value == null || value.isEmpty) {
+//   return 'Please enter some text';
+//   if it does not matches the pattern, like
+// it not contains @
+// } else if (!regExp.hasMatch(value)) {
+//   return 'Please enter a valid email address';
+// }
+// },
+// );
+// }
 
-  // TextFormField buildPasswordField() {
-  //   return TextFormField(
-  //     // obscureText: _passwordVisible,
-  //     textAlign: TextAlign.start,
-  //     keyboardType: TextInputType.visiblePassword,
-  //     style: kInputTextStyle,
-  //     decoration: InputDecoration(
-  //       labelText: 'Password',
-  //       floatingLabelBehavior: FloatingLabelBehavior.always,
-  //       suffixIcon: IconButton(
-  //         onPressed: () {
-  //           setState(() {
-  //             // _passwordVisible = !_passwordVisible;
-  //           });
-  //         },
-  //         // icon: Icon(
-  //           // _passwordVisible
-  //           //     ? Icons.visibility_off_outlined
-  //               : Icons.visibility_off_outlined,
-  //         // ),
-  //         iconSize: kDefaultPadding,
-  //       ),
-  //     ),
-  //     validator: (value) {
-  //       if (value!.length < 5) {
-  //         return 'Must be more than 5 characters';
-  //       }
-  //     },
-  //   );
-  // }
+// TextFormField buildPasswordField() {
+//   return TextFormField(
+//     // obscureText: _passwordVisible,
+//     textAlign: TextAlign.start,
+//     keyboardType: TextInputType.visiblePassword,
+//     style: kInputTextStyle,
+//     decoration: InputDecoration(
+//       labelText: 'Password',
+//       floatingLabelBehavior: FloatingLabelBehavior.always,
+//       suffixIcon: IconButton(
+//         onPressed: () {
+//           setState(() {
+//             // _passwordVisible = !_passwordVisible;
+//           });
+//         },
+//         // icon: Icon(
+//           // _passwordVisible
+//           //     ? Icons.visibility_off_outlined
+//               : Icons.visibility_off_outlined,
+//         // ),
+//         iconSize: kDefaultPadding,
+//       ),
+//     ),
+//     validator: (value) {
+//       if (value!.length < 5) {
+//         return 'Must be more than 5 characters';
+//       }
+//     },
+//   );
+// }
 }
 
-  // }
-
+// }
