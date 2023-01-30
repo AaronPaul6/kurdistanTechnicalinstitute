1c2ec049126d7a15e51cbe5d0e1f3552c4bc1345
Hazhan Jalal <hazhan@mail.com>
2023-01-30 14:06:49 +0300

    Login page buttons are responsive now and react to button click.s

--- lib/components/custom_buttons.dart
+++ lib/components/custom_buttons.dart
@@ -13,36 +13,38 @@ class DefaultButton extends StatelessWidget {
       required this.title,
       required this.iconData})
       : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onPress,
-      child: Container(
-        padding: EdgeInsets.only(right: kDefaultPadding),
-        width: 100.w,
-        height: SizerUtil.deviceType == DeviceType.tablet ? 9.h : 7.h,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [kSecondaryColor, kPrimaryColor],
-              begin: const FractionalOffset(0.0, 0.0),
-
-              end: const FractionalOffset(0.5, 0.0),
-              stops: [0.0, 1.0],
-              tileMode: TileMode.clamp,
-            ),
-            borderRadius: BorderRadius.circular(kDefaultPadding)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Spacer(),
-            Text(title, style: Theme.of(context).textTheme.subtitle2),
-            Spacer(),
-            Icon(
-              iconData,
-              size: 26.sp,
-              color: kOtherColor,
-            )
-          ],
+    return Material(
+      child: InkWell(
+        onTap: onPress,
+        child: Ink(
+          padding: EdgeInsets.only(right: kDefaultPadding),
+          width: 100.w,
+          height: SizerUtil.deviceType == DeviceType.tablet ? 9.h : 7.h,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [kSecondaryColor, kPrimaryColor],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(0.5, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp,
+              ),
+              borderRadius: BorderRadius.circular(kDefaultPadding)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              Text(title, style: Theme.of(context).textTheme.subtitle2),
+              Spacer(),
+              Icon(
+                iconData,
+                size: 26.sp,
+                color: kOtherColor,
+              )
+            ],
+          ),
         ),
       ),
     );
