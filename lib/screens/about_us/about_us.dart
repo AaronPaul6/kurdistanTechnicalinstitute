8b4148ce1e7efa1a0cf430d829323d7bc065d313
Arya Burhan <99354272+AaronPaul6@users.noreply.github.com>
2023-02-01 10:36:07 +0300

    commit_arya

--- lib/screens/about_us/about_us.dart
+++ lib/screens/about_us/about_us.dart
@@ -1,15 +1,4 @@
-import 'package:brain_school/constants.dart';
-import 'package:brain_school/screens/my_profile/my_profile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:sizer/sizer.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:brain_school/components/custom_buttons.dart';
-import 'package:brain_school/constants.dart';
-import 'package:brain_school/screens/home_screen/home_screen.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:sizer/sizer.dart';
 
 class AboutUs extends StatefulWidget {
   static String routeName = 'About_Us';
