85c43c2028fbb324ffc25e5751a3404a8e688ce9
Arya Burhan <99354272+AaronPaul6@users.noreply.github.com>
2023-02-19 12:26:55 +0300

    Revert "Arya's Commit"
    This reverts commit 568082077bb1f9cf56b5e6c3408977589009c234.

--- lib/screens/Contactus/contact.dart
+++ lib/screens/Contactus/contact.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
-// enum SocialMedia{facebook, twitter, email, instagram}
+enum SocialMedia{facebook, twitter, email, instagram}
 
-// final String _phoneNumber = '009647717028282' ;
-// final Uri _url =Uri.parse('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi52s2D-5H9AhV_YPEDHZ8FA-gQFnoECBIQAQ&url=https%3A%2F%2Fwww.facebook.com%2Fktiinstitute%2F&usg=AOvVaw0RQUWMdYVFepaV9IRSo9OO');
-// final urls = {
-//   // SocialMedia.facebook:
-//       'https://www.facebook.com/ktiinstitute/',
-// };
-// final url = {
-//   SocialMedia.instagram:
-//   'https://www.instagram.com/ktiinstitute/?hl=en',
-// };
+final String _phoneNumber = '009647717028282' ;
+final Uri _url =Uri.parse('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi52s2D-5H9AhV_YPEDHZ8FA-gQFnoECBIQAQ&url=https%3A%2F%2Fwww.facebook.com%2Fktiinstitute%2F&usg=AOvVaw0RQUWMdYVFepaV9IRSo9OO');
+final urls = {
+  SocialMedia.facebook:
+      'https://www.facebook.com/ktiinstitute/',
+};
+final url = {
+  SocialMedia.instagram:
+  'https://www.instagram.com/ktiinstitute/?hl=en',
+};
 void main() {
   runApp(const contact());
 }
@@ -74,95 +74,95 @@ class contact extends StatelessWidget {
               height: 40,
             ),
 
-  //          Row(
-  //            mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //            children:[
-  //              Container(
-  //                child: MaterialButton(
-  //                  padding: EdgeInsets.all(35),
-  //                  color: Colors.blue.shade800,
-  //                  minWidth: 100,
-  //                  height: 60,
-  //                  child: Align(
-  //                    alignment: Alignment.centerLeft,
-  //                    child:Icon(
-  //                      Icons.phone, size: 60, color: Colors.white,
-  //                    ),
-  //                  ),
-  //                  onPressed: ( ) async{
-  //                    // final _call='tel:$_phoneNumber';
-  //                  },
-  //
-  //                ),
-  //              ),
-  //
-  //              Container(
-  //                child: MaterialButton(
-  //                  padding: EdgeInsets.all(35),
-  //                  color: Colors.blue.shade800,
-  //                  minWidth: 100,
-  //                  height: 60,
-  //                  child: Align(
-  //                    alignment: Alignment.centerLeft,
-  //                    child:Icon(
-  //                      Icons.email, size: 60, color: Colors.white,
-  //                    ),
-  //                  ),
-  //                  onPressed: ( ) {
-  //                    launchUrl('mailto:marfsaman12345@gmail.com' as Uri);
-  //                  },
-  //
-  //                ),
-  //              ),
-  //
-  //            ],//children
-  //          ),
-  //       SizedBox(height: 40,),
-  //
-  //       Row(
-  //         mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //         children:[
-  //           Container(
-  //             child: MaterialButton(
-  //               padding: EdgeInsets.all(35),
-  //               color: Colors.blue.shade800,
-  //               minWidth: 100,
-  //               height: 60,
-  //               child: Align(
-  //                 alignment: Alignment.centerLeft,
-  //                 child:Icon(
-  //                   Icons.facebook_outlined, size: 60, color: Colors.white,
-  //                 ),
-  //               ),
-  //               onPressed: ( ) {
-  //                 launchUrl('https://www.facebook.com/ktiinstitute/' as Uri);
-  //               },
-  //
-  //             ),
-  //           ),
-  //
-  //           Container(
-  //             child: MaterialButton(
-  //               padding: EdgeInsets.all(35),
-  //               color: Colors.blue.shade800,
-  //               minWidth: 100,
-  //               height: 60,
-  //               child: Align(
-  //                 alignment: Alignment.centerLeft,
-  //                 child:Icon(
-  //                   Icons.camera, size: 60, color: Colors.white,
-  //                 ),
-  //               ),
-  //               onPressed: ( ) {
-  //                 launchUrl('https://www.instagram.com/ktiinstitute/?hl=en' as Uri);
-  //               },
-  //
-  //             ),
-  //           ),
-  //
-  // ],
-  //
-  //     ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children:[
+               Container(
+                 child: MaterialButton(
+                   padding: EdgeInsets.all(35),
+                   color: Colors.blue.shade800,
+                   minWidth: 100,
+                   height: 60,
+                   child: Align(
+                     alignment: Alignment.centerLeft,
+                     child:Icon(
+                       Icons.phone, size: 60, color: Colors.white,
+                     ),
+                   ),
+                   onPressed: ( ) async{
+                     final _call='tel:$_phoneNumber';
+                   },
+
+                 ),
+               ),
+
+               Container(
+                 child: MaterialButton(
+                   padding: EdgeInsets.all(35),
+                   color: Colors.blue.shade800,
+                   minWidth: 100,
+                   height: 60,
+                   child: Align(
+                     alignment: Alignment.centerLeft,
+                     child:Icon(
+                       Icons.email, size: 60, color: Colors.white,
+                     ),
+                   ),
+                   onPressed: ( ) {
+                     launchUrl('mailto:marfsaman12345@gmail.com' as Uri);
+                   },
+
+                 ),
+               ),
+
+             ],//children
+           ),
+        SizedBox(height: 40,),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children:[
+            Container(
+              child: MaterialButton(
+                padding: EdgeInsets.all(35),
+                color: Colors.blue.shade800,
+                minWidth: 100,
+                height: 60,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child:Icon(
+                    Icons.facebook_outlined, size: 60, color: Colors.white,
+                  ),
+                ),
+                onPressed: ( ) {
+                  launchUrl('https://www.facebook.com/ktiinstitute/' as Uri);
+                },
+
+              ),
+            ),
+
+            Container(
+              child: MaterialButton(
+                padding: EdgeInsets.all(35),
+                color: Colors.blue.shade800,
+                minWidth: 100,
+                height: 60,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child:Icon(
+                    Icons.camera, size: 60, color: Colors.white,
+                  ),
+                ),
+                onPressed: ( ) {
+                  launchUrl('https://www.instagram.com/ktiinstitute/?hl=en' as Uri);
+                },
+
+              ),
+            ),
+
+  ],
+
+      ),
 
 ],
         ),
